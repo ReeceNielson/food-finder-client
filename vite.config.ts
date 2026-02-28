eccd0953fc376f4b5b94edd1806beb5ce46aac16
Mark Soulier <mark.soulier@icloud.com>
2026-02-28 11:50:45 -0700

    changes for icon and presentation

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "/food-finder-client/",
 	plugins: [react(), tailwindcss()],
 	server: {
 		// tailscale
